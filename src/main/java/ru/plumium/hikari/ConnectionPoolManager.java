470d6039595acf8d0e84253e9a2b236f10025b54
vedro </>
2022-03-07 03:56:19 +0400

    Первый коммит

---
+++ src/main/java/ru/plumium/hikari/ConnectionPoolManager.java
@@ -0,0 +1,80 @@
+package ru.plumium.hikari;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectionPoolManager {
+    private final PlumiumHikari plugin;
+
+    private HikariDataSource dataSource;
+
+    private String hostname;
+    private String port;
+    private String database;
+    private String username;
+    private String password;
+    private int minimumConnections;
+    private int maximumConnections;
+    private int connectionTimeout;
+    private String testQuery;
+
+    public ConnectionPoolManager(PlumiumHikari plugin) {
+        this.plugin = plugin;
+        init();
+        setupPool();
+    }
+
+    private void init() {
+        hostname = plugin.getConfig().getString("sql.hostname");
+        port = plugin.getConfig().getString("sql.port");
+        database = plugin.getConfig().getString("sql.database");
+        username = plugin.getConfig().getString("sql.username");
+        password = plugin.getConfig().getString("sql.password");
+        minimumConnections = plugin.getConfig().getInt("sql.connectionsMin");
+        maximumConnections = plugin.getConfig().getInt("sql.connectionsMax");
+        connectionTimeout = plugin.getConfig().getInt("sql.connectionsTimeout");
+        testQuery = "SELECT 1;";
+    }
+
+    private void setupPool() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.jdbc.Driver");
+        config.setJdbcUrl(
+                "jdbc:mysql://" +
+                        hostname +
+                        ":" +
+                        port +
+                        "/" +
+                        database +
+                        "?serverTimezone=UTC&rewriteBatchedStatements=true&characterEncoding=utf8"
+        );
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setMinimumIdle(minimumConnections);
+        config.setMaximumPoolSize(maximumConnections);
+        config.setConnectionTimeout(connectionTimeout);
+        config.setConnectionTestQuery(testQuery);
+        dataSource = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public void close(Connection conn, PreparedStatement ps, ResultSet res) {
+        if (conn != null) try { conn.close(); } catch (SQLException ignored) {}
+        if (ps != null) try { ps.close(); } catch (SQLException ignored) {}
+        if (res != null) try { res.close(); } catch (SQLException ignored) {}
+    }
+
+    public void closePool() {
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
+    }
+}
---
+++ src/main/java/ru/plumium/hikari/PlumiumHikari.java
@@ -0,0 +1,51 @@
+package ru.plumium.hikari;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PlumiumHikari extends JavaPlugin {
+    private FileConfiguration config;
+    private ConnectionPoolManager hikariPool;
+
+    public @NotNull FileConfiguration getConfig() {
+        return config;
+    }
+
+    @Override
+    public void onEnable() {
+        loadConfig();
+        hikariPool = new ConnectionPoolManager(this);
+    }
+
+    @Override
+    public void onDisable() {
+        hikariPool.closePool();
+    }
+
+    private void loadConfig() {
+        File configFile = new File(getDataFolder() + File.separator + "config.yml");
+        if (!configFile.exists()) {
+            Bukkit.getLogger().warning("Файл конфигурации не обнаружен, создаём дефолтный.");
+            this.saveDefaultConfig();
+            config = YamlConfiguration.loadConfiguration(configFile);
+        }
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public Connection getHikariConnection() throws SQLException {
+        return hikariPool.getConnection();
+    }
+
+    public void closeHikariConnection(Connection conn, PreparedStatement ps, ResultSet res) {
+        hikariPool.close(conn, ps, res);
+    }
+}
