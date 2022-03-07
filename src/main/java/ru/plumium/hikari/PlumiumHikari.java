fc89e445803f0ff33ff9991d9b451c4b6efbe88c
vedro </>
2022-03-07 04:45:07 +0400

    Попытка #5 исправления ошибки

--- src/main/java/ru/plumium/hikari/PlumiumHikari.java
+++ src/main/java/ru/plumium/hikari/PlumiumHikari.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 
 public class PlumiumHikari extends JavaPlugin {
     private FileConfiguration config;
-    private ConnectionPoolManager hikariPool;
+    private static ConnectionPoolManager hikariPool;
 
     public @NotNull FileConfiguration getConfig() {
         return config;
